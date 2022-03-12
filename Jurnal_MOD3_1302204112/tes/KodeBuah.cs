7158ddb971183e64bf4e45ade347452915f4f91b
rifkyfirdaus28 <100988194+rifkyfirdaus28@users.noreply.github.com>
2022-03-12 14:59:46 +0700

    Add files via upload

---
+++ Jurnal_MOD3_1302204112/tes/KodeBuah.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tes
+{
+    internal class KodeBuah
+    {
+        internal class Kodebuah
+        {
+            string[] namabuah = { "Apel", "Aprikot", "Alpukat", "Pisang", "Paprika", "Blackberry", "Ceri", "Kelapa", "Jagung", "Kurma", "Durian", "Anggur", "Melon", "Semangka" };
+            string[] kodebuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "O00" };
+
+            public void getKodeBuah()
+            {
+                int panjang = namabuah.Length;
+                for (int i = 0; i < panjang; i++)
+                {
+                    Console.WriteLine(namabuah[i]);
+                }
+            }
+        }
+    }
+}
